499833c379e7ee81170fe1ed4096e09d37bd138b
cheesetan <93832702+cheesetan@users.noreply.github.com>
2023-01-25 19:39:17 +0800

    Initial Commit

---
+++ CCAttendance/CCAttendanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCAttendanceApp.swift
+//  CCAttendance
+//
+//  Created by Tristan on 25/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CCAttendanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
