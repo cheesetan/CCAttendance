3f27587b9735038a3f8edb9da0792b13d0568f7d
cheesetan <93832702+cheesetan@users.noreply.github.com>
2023-01-25 22:08:25 +0800

    Added UI for Sign In

--- CCAttendance/Authentication.swift
+++ CCAttendance/Authentication.swift
@@ -6,31 +6,95 @@
 //
 
 import SwiftUI
+import SwiftUI
 import Firebase
 import FirebaseAuth
 
 struct Authentication: View {
     
+    let generator = UIImpactFeedbackGenerator(style: .medium)
+    let generator2 = UINotificationFeedbackGenerator()
+    
     @State var email = String()
+    @State var password = String()
+    @State var isSecured = true
     
     var body: some View {
-        VStack {
+        VStack(spacing: 15) {
             Spacer()
             
-            Text("welcome - placeholder")
-                .fontWeight(.bold)
-                .font(.title3)
+            HStack {
+                Text("Sign in to CCAttendance")
+                    .fontWeight(.bold)
+                    .font(.title2)
+                Spacer()
+            }
             
             emailField
             
+            passwordField
+            
             Spacer()
         }
+        .padding(.horizontal)
     }
     
     
     var emailField: some View {
         VStack {
-            TextField("", text: $email)
+            HStack {
+                Text("SST Email")
+                    .fontWeight(.bold)
+                    .font(.caption)
+                Spacer()
+            }
+            
+            TextField("SST Email", text: $email)
+                .padding(20)
+                .background(.thinMaterial)
+                .cornerRadius(10)
+                .textInputAutocapitalization(.never)
+        }
+    }
+    
+    var passwordField: some View {
+        VStack {
+            HStack {
+                Text("Password")
+                    .fontWeight(.bold)
+                    .font(.caption)
+                Spacer()
+            }
+            
+            ZStack(alignment: .trailing) {
+                Group {
+                    if isSecured {
+                        SecureField("Password", text: $password)
+                            .padding(20)
+                            .background(.thinMaterial)
+                            .cornerRadius(10)
+                    } else {
+                        TextField("Password", text: $password)
+                            .padding(20)
+                            .background(.thinMaterial)
+                            .cornerRadius(10)
+                    }
+                    Spacer()
+                    Button(action: {
+                        isSecured.toggle()
+                        generator.impactOccurred(intensity: 0.5)
+                    }) {
+                        Image(systemName: self.isSecured ? "eye.slash" : "eye")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 23, height: 23)
+                            .accentColor(.gray)
+                            .padding()
+                            .padding(.trailing, 3)
+                    }
+                    
+                }
+            }
         }
     }
 }
